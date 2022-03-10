a9daee9077ffbb3e9d7b54dbc8bd210e1ba18b30
Imogen Kinsman <jade@thezets.com>
2022-03-09 20:31:47 -0800

    start to wire in websockt

--- lib/ex_live_chat/chat.ex
+++ lib/ex_live_chat/chat.ex
@@ -8,4 +8,12 @@ defmodule ExLiveChat.Chat do
   # def subscribe(user_id) do
   #   Phoenix.PubSub.subscribe(Demo.PubSub, @topic <> "#{user_id}")
   # end
+
+  defp notify_subscribers({:ok, result}, event) do
+    Phoenix.PubSub.broadcast(ExLiveChat.PubSub, @topic, {__MODULE__, event, result})
+    {:ok, result}
+  end
+
+  # idk could do something with this probably
+  defp notify_subscribers({:error, reason}, _event), do: {:error, reason}
 end
--- lib/ex_live_chat_web/live/chat_live.ex
+++ lib/ex_live_chat_web/live/chat_live.ex
@@ -2,12 +2,21 @@ defmodule ExLiveChatWeb.ChatLive do
   use ExLiveChatWeb, :live_view
   use Phoenix.HTML
 
+  # alias Phoenix.LiveView.Socket
+  alias ExLiveChat.Chat
+
   def mount(_params, _session, socket) do
     socket = assign(socket, :chat, [["immy", "meow"]])
     # socket = assign(socket, %{message: "msg"}, [])
     # socket = assign(socket, )
     socket = assign(socket, :typing, false)
     {:ok, socket}
+  end
+
+  # this can probably be moved into mount unless I need specific HTTP parameters
+  def handle_params(%{"id" => _id}, _url, socket) do
+    if connected?(socket), do: Chat.subscribe()
+    {:noreply, socket}
   end
 
   def render(assigns) do
@@ -20,8 +29,11 @@ defmodule ExLiveChatWeb.ChatLive do
   # end
 
   def handle_event("say", %{"name" => name, "text" => text}, socket) do
-    chat = [[name, text] | socket.assigns.chat]
+    # chat = [[name, text] | socket.assigns.chat]
+    chat = socket.assigns.chat
     IO.inspect(chat)
+    Chat.say()
+
     {:noreply, assign(socket, :chat, chat)}
   end
 
